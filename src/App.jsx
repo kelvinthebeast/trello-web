b86171c89343f0b82c492bd8e8e37cde717ffc77
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-02-09 16:34:45 +0700

    add dark mode light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { Box, TextField, Button } from '@mui/material'
-// import { useColorScheme } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
 
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <>
+      <ModeToggle/>
+      <br />
       <Button>CLick the link to have s*x </Button>
       <div>My name is Kelvin the beast</div>
       <Typography variant="body2" color="text.secondary">100000$</Typography>
